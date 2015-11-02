881a321fc3547863fdbbb7c336b87e9ea9e65429
Shaun O'Reilly <prosoftnz@gmail.com>
2015-11-03 02:23:40 +1030

    Initial Commit

---
+++ CoffeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeApp
+//
+//  Created by Shaun O'Reilly on 3/11/2015.
+//  Copyright © 2015 Visual Recruit Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoffeApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoffeApp
+//
+//  Created by Shaun O'Reilly on 3/11/2015.
+//  Copyright © 2015 Visual Recruit Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
